e9af2632407f452e56a8e099c990add16bcd1bd3
hieudt <hieudtx9@gmail.com>
2021-09-06 22:07:07 +0700

    merge merge

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,31 +1,30 @@
 import "./profile.css";
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import PropTypes from 'prop-types';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableFooter from '@material-ui/core/TableFooter';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableFooter from "@material-ui/core/TableFooter";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import FirstPageIcon from "@material-ui/icons/FirstPage";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import LastPageIcon from "@material-ui/icons/LastPage";
 const useStyles = makeStyles({
-  table: {
-  },
+  table: {}
 });
 const paggingStyles = makeStyles((theme) => ({
   root: {
     flexShrink: 0,
-    marginLeft: theme.spacing(2.5),
-  },
+    marginLeft: theme.spacing(2.5)
+  }
 }));
 function TablePaginationActions(props) {
   const classes = paggingStyles();
@@ -55,24 +54,37 @@ function TablePaginationActions(props) {
         disabled={page === 0}
         aria-label="first page"
       >
-        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
       </IconButton>
-      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
       </IconButton>
       <IconButton
         onClick={handleNextButtonClick}
         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
         aria-label="next page"
       >
-        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+        )}
       </IconButton>
       <IconButton
         onClick={handleLastPageButtonClick}
         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
         aria-label="last page"
       >
-        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
       </IconButton>
     </div>
   );
@@ -81,24 +93,24 @@ TablePaginationActions.propTypes = {
   count: PropTypes.number.isRequired,
   onPageChange: PropTypes.func.isRequired,
   page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired
 };
 export default function Profile() {
   const [data, setData] = useState({ data: [] });
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        'https://my-json-server.typicode.com/typicode/demo/posts',
-      )
+        "https://my-json-server.typicode.com/typicode/demo/posts"
+      );
       setData(result.data);
     };
     fetchData();
-
   }, []);
   const classes = useStyles();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
@@ -109,15 +121,18 @@ export default function Profile() {
   return (
     <div className="Login">
       {/* table with pagination */}
-      {
-        localStorage.getItem('user')&&
-        <h4 className="user-name">Wellcome <b>{JSON.parse(localStorage.getItem('user')).name}</b></h4>
-      }
+      {localStorage.getItem("user") && (
+        <h4 className="user-name">
+          Wellcome <b>{JSON.parse(localStorage.getItem("user")).name}</b>
+        </h4>
+      )}
       <div className="table-result">
-        {
-          data.length > 0 &&
+        {data.length > 0 && (
           <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="custom pagination table">
+            <Table
+              className={classes.table}
+              aria-label="custom pagination table"
+            >
               <TableHead>
                 <TableRow>
                   <TableCell>#</TableCell>
@@ -127,7 +142,10 @@ export default function Profile() {
               </TableHead>
               <TableBody>
                 {(rowsPerPage > 0
-                  ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  ? data.slice(
+                      page * rowsPerPage,
+                      page * rowsPerPage + rowsPerPage
+                    )
                   : data
                 ).map((row, idx) => (
                   <TableRow key={row.id}>
@@ -148,14 +166,19 @@ export default function Profile() {
               <TableFooter>
                 <TableRow>
                   <TablePagination
-                    rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                    rowsPerPageOptions={[
+                      5,
+                      10,
+                      25,
+                      { label: "All", value: -1 }
+                    ]}
                     colSpan={3}
                     count={data.length}
                     rowsPerPage={rowsPerPage}
                     page={page}
                     SelectProps={{
-                      inputProps: { 'aria-label': 'rows per page' },
-                      native: true,
+                      inputProps: { "aria-label": "rows per page" },
+                      native: true
                     }}
                     onPageChange={handleChangePage}
                     onRowsPerPageChange={handleChangeRowsPerPage}
@@ -165,10 +188,9 @@ export default function Profile() {
               </TableFooter>
             </Table>
           </TableContainer>
-        }
+        )}
       </div>
       {/* end */}
     </div>
   );
 }
-
